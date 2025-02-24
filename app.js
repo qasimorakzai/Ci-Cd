3617a3d6f903cf856460ac7f3895e8bcefd73858
qasimorakzai <muhammad.qasimb3@gmail.com>
2025-02-24 18:49:40 +0500

    Update CI/CD pipeline to trigger on both main and qasim

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, testing ci/cd testing');
+  res.send('Hello my name is vinod bahador thapa');
 });
 
 const PORT = process.env.PORT || 5000;
