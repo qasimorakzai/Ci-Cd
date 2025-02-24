8e62441a26369c43c819c56cba547b63a60078bd
qasimorakzai <muhammad.qasimb3@gmail.com>
2025-02-24 18:33:36 +0500

    Update code to trigger CI/CD pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, from updated code');
+  res.send('Hello, how are you');
 });
 
 const PORT = process.env.PORT || 5000;
